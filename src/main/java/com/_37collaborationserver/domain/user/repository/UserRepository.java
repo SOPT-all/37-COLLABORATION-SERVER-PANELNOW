05fd5f7b254118c2412245e8d76097071d0963a1
woals2840 <woals2840@gmail.com>
2025-11-18 21:27:51 +0900

    feat:6 User조회 API 구현

--- src/main/java/com/_37collaborationserver/domain/user/repository/UserRepository.java
+++ src/main/java/com/_37collaborationserver/domain/user/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package com._37collaborationserver.domain.user.repository;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com._37collaborationserver.domain.user.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 }
