53aeafa6f11d675e16b39e9389958eab3d0f31a6
byunhm02 <byunhm02@gmail.com>
2025-11-19 19:16:46 +0900

    fix:#3 컬럼명 manual->usage_manual로 변경

--- src/main/java/com/_37collaborationserver/domain/product/entity/Product.java
+++ src/main/java/com/_37collaborationserver/domain/product/entity/Product.java
@@ -1,10 +1,6 @@
 package com._37collaborationserver.domain.product.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -29,7 +25,7 @@ public class Product {
 	@Column(nullable = false)
 	private String info;
 
-	@Column(nullable = false)
+	@Column(nullable = false,name = "usage_manual")
 	private String manual;
 
 	@Column(nullable = false)
