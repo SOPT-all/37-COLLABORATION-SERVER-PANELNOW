e3b0c26eed2e9d0ef8c3f4781ad981c7b8b714a0
byunhm02 <byunhm02@gmail.com>
2025-11-19 19:38:53 +0900

    feat:#3 단건조회 response dto생성

---
+++ src/main/java/com/_37collaborationserver/domain/product/dto/ProductItemResponse.java
@@ -0,0 +1,33 @@
+package com._37collaborationserver.domain.product.dto;
+
+import com._37collaborationserver.domain.product.entity.Product;
+import lombok.Builder;
+
+
+@Builder
+public record ProductItemResponse(
+        Long id,
+        String imageUrl,
+        String name,
+        int price,
+        String phoneNumber,
+        String exchangeDay,
+        String info,
+        String usageManual,
+        String guide
+
+) {
+    public static ProductItemResponse from(Product product,String phoneNumber) {
+        return ProductItemResponse.builder()
+                .id(product.getId())
+                .imageUrl(product.getImageUrl())
+                .name(product.getName())
+                .price(product.getPrice())
+                .phoneNumber(phoneNumber)
+                .exchangeDay(product.getExchangeDate())
+                .info(product.getInfo())
+                .usageManual(product.getManual())
+                .guide(product.getGuide())
+                .build();
+    }
+}
